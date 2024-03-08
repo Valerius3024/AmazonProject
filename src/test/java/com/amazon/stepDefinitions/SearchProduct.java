4e34c0f5d725750ac1fcadb35dfd5aad796ff39c
Mert <125090201+MerT-GH@users.noreply.github.com>
2024-03-08 14:20:10 +0300

    AMZ-03 | Changed SearchProduct StepDefinitions

--- src/test/java/com/amazon/stepDefinitions/SearchProduct.java
+++ src/test/java/com/amazon/stepDefinitions/SearchProduct.java
@@ -1,6 +1,5 @@
 package com.amazon.stepDefinitions;
 
-import com.amazon.pages.LoginPage;
 import com.amazon.pages.SearchProductPage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,19 +8,17 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 public class SearchProduct {
-
-    LoginPage loginPage = new LoginPage();
     SearchProductPage searchProductPage = new SearchProductPage();
 
     @When("the user searches by {string} in the search field")
-    public void the_user_searches_by_in_the_search_field(String productName) throws InterruptedException {
+    public void the_user_searches_by_in_the_search_field(String productName){
         searchProductPage.searchField.sendKeys(productName + Keys.ENTER);
     }
 
     @Then("the search result should show minimum one product whose name includes {string}")
     public void the_search_result_should_show_minimum_one_product_whose_name_includes(String productName) {
         for (WebElement webElement : searchProductPage.productList) {
-            if (webElement.getText().contains(productName)){
+            if (webElement.getText().toLowerCase().contains(productName)){
                 Assert.assertTrue(true);
                 break;
             }else{
